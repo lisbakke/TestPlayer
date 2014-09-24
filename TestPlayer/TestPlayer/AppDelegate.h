53d1bb976b740411230fe9504b02de3ba47d7b1f
Ben Lisbakken <lisbakke@gmail.com>
2014-09-24 16:36:22 -0600

    First.

---
+++ TestPlayer/TestPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestPlayer
+//
+//  Created by BENJAMIN LISBAKKEN on 8/6/14.
+//  Copyright (c) 2014 BENJAMIN LISBAKKEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestPlayer/TestPlayer/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  TestPlayer
+//
+//  Created by BENJAMIN LISBAKKEN on 8/6/14.
+//  Copyright (c) 2014 BENJAMIN LISBAKKEN. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  // Override point for customization after application launch.
+  self.window.backgroundColor = [UIColor whiteColor];
+  [self.window makeKeyAndVisible];
+  return YES;
+}
+
+@end
